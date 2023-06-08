704f5d01416ab68dc3cafc6d348e4a0617c3b828
hunghx <baby10051999@gmail.com>
2023-06-08 10:36:05 +0700

    update

--- src/Demo.java
+++ src/Demo.java
@@ -2,6 +2,13 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Demo {
+    int i;
+    public Demo(String str){
+
+    }
+    public Demo(){
+
+    }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         char nhapvao =  sc.nextLine().charAt(0); //  hung => h
@@ -16,4 +23,12 @@ public class Demo {
         float f = 1.3455644675f;
         System.out.printf("ky tu %c xuat hien %-20d lan o trong chuoi %-50s ,date %-40s, so thuc : %-10.3f " ,nhapvao,count,str,date,f );
     }
+    public void display(){
+        this.i = 10;
+        System.out.println("helo");
+    }
+     public static Scanner scanner(){
+       return new Scanner(System.in);
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,53 @@
 import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
+
     public static void main(String[] args) {
         // Khai báo mảng
-        int[] arrInt = {1, 2, 3, 4};
-        int arrInt1[];
-        System.out.println("Mảng ban đầu =>>>" + Arrays.toString(arrInt));
+//        int[] arrInt = {1, 2, 3, 4};
+//        int arrInt1[];
+//        System.out.println("Mảng ban đầu =>>>" + Arrays.toString(arrInt));
 //        test(arrInt); // gán giá trị của arrInt cho  tham số arr
-        System.out.println("Mảng sau khi thay đổi =>>>" + Arrays.toString(arrInt));
-        int[] arrInt2 = new int[5];
-        System.out.println("Các giá trị chưa khởi tạo của mảng ArrInt2 là " + Arrays.toString(arrInt2));
-        String arrStr[] = new String[10];
-        System.out.println("Các giá trị chưa khởi tạo của mảng ArrInt2 là " + Arrays.toString(arrStr));
-        //lấy ra phần tử mảng arr[index]
-        System.out.println("Phần tử có index =1 trong arrInt là " + arrInt[1]);
+//        System.out.println("Mảng sau khi thay đổi =>>>" + Arrays.toString(arrInt));
+//        int[] arrInt2 = new int[5];
+//        System.out.println("Các giá trị chưa khởi tạo của mảng ArrInt2 là " + Arrays.toString(arrInt2));
+//        String arrStr[] = new String[10];
+//        System.out.println("Các giá trị chưa khởi tạo của mảng ArrInt2 là " + Arrays.toString(arrStr));
+//        //lấy ra phần tử mảng arr[index]
+//        System.out.println("Phần tử có index =1 trong arrInt là " + arrInt[1]);
         // thay đổi giá trị của phần tử mảng
-        arrInt[2] = 100;
-        System.out.println("Phần tử có index = 2 trong arrInt là " + arrInt[2]);
+//        arrInt[2] = 100;
+//        System.out.println("Phần tử có index = 2 trong arrInt là " + arrInt[2]);
 
         // duyệt mảng
-        // fori
-        for (int i = 0; i < arrInt.length; i++) {
-            int element = arrInt[i];
-            // làm việc với phần tử tại vị trí i
-            System.out.println(element
-            );
-        }
+        // fori // thay đổi giá trị mảng
+//        for (int i = 0; i < arrInt.length; i++) {
+//            int element = arrInt[i];
+//            // làm việc với phần tử tại vị trí i
+//            System.out.println(element
+//            );
+//        }
         // for each
-        for (int e : arrInt) {
-            System.out.println(e);
-        }
+//        for (int e : arrInt) {
+//            System.out.println(e);
+//        }
+        // foreach
+//        String[] arrString = {"banana","kandy","Thanh dragon"};
+//        for (String str:arrString) {
+//            System.out.println("Quả "+ str);
+//        }
+//        System.out.println("Dộ dài của mảng "+arrString.length);
+//        System.out.println("Dộ dài của mảng "+arrInt2.length);
+//        // index
+//        arrString[1] ="Măng cụt";
+//        System.out.println(arrString[1]);
+//        // Phương thức mặc định
+//        System.out.println(Math.pow(2,9));
+//        System.out.println(Math.ceil(Math.random()*100));
+//        // lớp Radom
+//        int radom = new Random().nextInt(100);
+//        System.out.println("Radom  "+ radom);
         // khác nhau giữa static và ko có static
 //        Test t = new Test();
 //        t.print();
@@ -39,12 +57,61 @@ public class Main {
 //        System.out.println("Kết quả của 8 cho 0 là "+ result);
         // {1,2,3,4,5,6,7,8,9}
         // chèn cố 10 vào giữa số 4 và 5 (index = 5)
-        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-        int b = 10;
-        int index = 5;
-        int arrUpdate[] = new int[10];
-        insertArray(array, b, index, arrUpdate);
-        System.out.println("Array " + Arrays.toString(arrUpdate));
+//        int array[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+//        int b = 10;
+//        int index = 5;
+//        int arrUpdate[] = new int[10];
+//        insertArray(array, b, index, arrUpdate);
+//        System.out.println("Array " + Arrays.toString(arrUpdate));
+//        Demo.display();
+//        Demo demo =new Demo();
+//        Demo demo1 =new Demo("hùng");
+//        demo.display();
+
+
+        // Máy nhập
+//        System.out.println("dòng 1");
+//        Demo.scanner().nextLine();
+//        System.out.println("dòng 2");
+//        Demo.scanner().nextInt();
+//        System.out.println("dòng 3");
+//        Demo.scanner().nextLine();
+//        System.out.println("dòng 4");
+//        Demo.scanner().nextFloat();
+//        System.out.println("dòng 5");
+//        Demo.scanner().nextLine();
+//        System.out.println("dòng 6");
+//        Demo.scanner().nextInt();
+        // mảng 2 chiều
+
+        System.out.println("nhâp vào i và j");
+        int r = Demo.scanner().nextInt();
+        int c =Demo.scanner().nextInt();
+        int[][] arr1 = new int[r][c];
+//        System.out.println(arr1[2][1]);
+        System.out.println(arr1.length);
+        for (int i = 0; i < r  ; i++) {
+            for (int j = 0; j < c; j++) {
+                arr1[i][j] = 10;
+            }
+        }
+
+        for (int i = 0; i < r  ; i++) {
+            System.out.println(Arrays.toString(arr1[i]));
+        }
+        for (int i = 0; i < r  ; i++) {
+            for (int j = 0; j < c; j++) {
+                System.out.print(" "+arr1[i][j]+" ");
+            }
+            System.out.println();
+        }
+        // duyệt mảng
+//        for (int i = 0; i < 6; i++) {
+//            for (int j = 0; j <8 ; j++) {
+//
+//            }
+//        }
+
     }
 
     public static void test(int[] arr) {
